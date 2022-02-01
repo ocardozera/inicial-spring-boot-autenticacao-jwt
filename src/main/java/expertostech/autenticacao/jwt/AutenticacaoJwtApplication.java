72d41d2c6d4b63e7b2f74f860e02b3bf13f86f41
Matheus Cardoso <matheusjosecardoso@outlook.com>
2022-02-01 19:45:49 -0300

    Commit projeto que funciona JWT

--- src/main/java/expertostech/autenticacao/jwt/AutenticacaoJwtApplication.java
+++ src/main/java/expertostech/autenticacao/jwt/AutenticacaoJwtApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class AutenticacaoJwtApplication {
 
 	public static void main(String[] args) {
@@ -17,6 +17,7 @@ public class AutenticacaoJwtApplication {
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//		System.out.println(encoder.encode("@1234"));
 		return encoder;
 	}
 
